03986d8bd630ea7e3e9ef8dff92f15720f4215a2
shadowyWeb3SuperCoder@protonmail.com <shadowyWeb3SuperCoder@protonmail.com>
2022-04-22 21:41:39 -0300

    clean up

--- go/go.mod
+++ go/go.mod
@@ -3,11 +3,17 @@ module github.com/sonsofcrypto/web3lib
 go 1.17
 
 require (
-	github.com/ethereum/go-ethereum v1.10.17
-	github.com/sonsofcrypto/bip39 v0.0.0-20220421113407-ecb05db8f0cf // master
+	github.com/sonsofcrypto/bip39 master
+	github.com/sonsofcrypto/keyStore v0.0.0-20220423001258-ffab8dfa4bac
 )
 
+require golang.org/x/mobile v0.0.0-20190719004257-d2bd2a29d028
+
 require (
+	github.com/btcsuite/btcd/btcec/v2 v2.1.2 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/ethereum/go-ethereum v1.10.17 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20220331220935-ae2d96664a29 // indirect
-	golang.org/x/mobile v0.0.0-20220407111146-e579adbbc4a2 // indirect
+	golang.org/x/sys v0.0.0-20210816183151-1e6c022a8912 // indirect
 )
--- go/web3lib.go
+++ go/web3lib.go
@@ -1,11 +1,33 @@
+// SPDX-License-Identifier: MIT
 package web3lib
 
 import (
+	"encoding/hex"
 	"fmt"
-	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/sonsofcrypto/bip39"
+	keyStore "github.com/sonsofcrypto/keyStore"
+	_ "golang.org/x/mobile/bind"
 	"log"
 )
+
+// SomeType is for testing
+type SomeType struct {
+	foo string
+}
+
+// NewSomeType create new instance
+func NewSomeType() *SomeType {
+	return &SomeType{
+		foo: "SomeType",
+	}
+}
+
+// NewKeyStore create new default keyStore
+func NewKeyStore() *keyStore.KeyStore {
+	return keyStore.NewKeyStore(
+		[]keyStore.Backend{keyStore.NewDiskBackEnd("~/.soc/keystore")},
+	)
+}
 
 //HelloWorld export Hello world
 func HelloWorld() {
@@ -27,7 +49,7 @@ func NewWrapperMnemonic(wordListPath string) string {
 	// Generate seed from mnemonic
 	seed := bip39.NewSeed(mnemonic, "Secret Passphrase")
 
-	fmt.Println("Hex:", hexutil.Encode(seed))
+	fmt.Println("Hex:", hex.EncodeToString(seed))
 
 	return mnemonic
 }
